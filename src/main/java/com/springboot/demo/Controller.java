5bd31fefcbd3f79d4f21b47bef17b46b61255cad
sahitya4ud <sahitya4ud@gmail.com>
2024-10-06 20:51:38 -0500

    springboot

---
+++ src/main/java/com/springboot/demo/Controller.java
@@ -0,0 +1,15 @@
+package com.springboot.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping(value = "api/hello",consumes = "application/json",produces = "application/json")
+    public String getAccess(){
+        System.out.println("hello world");
+        return "hello";
+
+    }
+
+}
